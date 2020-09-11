bd61b894cafb4d1ec1d24ac2bd9bae02a95787e3
Abdul Samad <Samk8778@yahoo.com>
2020-09-11 18:25:25 +0530

    :lipstick: Switched to heroicons (pure svg tags)

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -86,20 +86,31 @@ function Home() {
 			<div
 				onClick={() => setSettingsOpen(!settingsOpen)}
 				className='absolute top-0 right-0 p-5 text-white z-30'>
-				<i
-					className={`my-5 transition-all duration-300 fa-lg ${
-						settingsOpen ? 'fas fa-times' : 'fas fa-bars'
-					}`}></i>
+				<svg
+					className='w-6 h-6'
+					fill='currentColor'
+					viewBox='0 0 20 20'
+					xmlns='http://www.w3.org/2000/svg'>
+					<path
+						fillRule='evenodd'
+						d={
+							settingsOpen
+								? 'M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z'
+								: 'M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z'
+						}
+						clipRule='evenodd'
+					/>
+				</svg>
 			</div>
 
 			{/* Home */}
 			<div
+				className='home h-screen w-screen flex flex-col justify-between p-5 text-white'
 				style={{
 					background: backgroundImageURL
 						? `url(${backgroundImageURL}) center / cover no-repeat`
 						: 'linear-gradient(to top, #3a1c71, #d76d77, #ffaf7b)',
-				}}
-				className='home h-screen w-screen flex flex-col justify-between p-5 text-white'>
+				}}>
 				<animated.section style={slideDown} className='mt-5 mb-3 z-10'>
 					<h2 className='text-xl font-bold'>{name}</h2>
 					<h2 className='text-6xl'>{temp}&deg;</h2>
