534a3f123902b39ebc05f84820332f17c520bca8
Abdul Samad <Samk8778@yahoo.com>
2020-09-13 23:35:24 +0530

    :lipstick: Reduced wind icon height

--- src/components/pages/Next48Hours.js
+++ src/components/pages/Next48Hours.js
@@ -43,17 +43,19 @@ function Next48Hours() {
 											</span>
 										</div>
 										<div className='w-1/4 flex flex-col items-center text-center my-1 text-lg'>
-											<svg
-												xmlns='http://www.w3.org/2000/svg'
-												width='24'
-												height='24'
-												viewBox='0 0 24 24'
-												className='h-8 fill-current'
-												style={{
-													transform: `rotateZ(${wind_deg}deg)`,
-												}}>
-												<path d='M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z' />
-											</svg>
+											<div className='h-8'>
+												<svg
+													xmlns='http://www.w3.org/2000/svg'
+													width='24'
+													height='24'
+													viewBox='0 0 24 24'
+													className='w-6 h-6 fill-current'
+													style={{
+														transform: `rotateZ(${wind_deg}deg)`,
+													}}>
+													<path d='M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z' />
+												</svg>
+											</div>
 											<span>
 												{unit === 'imperial'
 													? wind_speed + ' mi/hr'
