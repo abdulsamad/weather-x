d82aa4270fc408b2d8b919a600b33bed1b7b9f7d
Abdul Samad <Samk8778@yahoo.com>
2020-09-13 16:55:50 +0530

    :lipstick: Added optional more weather details

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -15,6 +15,7 @@ function Home() {
 		unit,
 		settingsOpen,
 		current: {
+			// dt,
 			feels_like,
 			humidity,
 			pressure,
@@ -24,15 +25,16 @@ function Home() {
 			weather,
 			wind_deg,
 			wind_speed,
-			// dt,
-			// clouds,
-			// dew_point,
-			// uvi,
-			// visibility,
+			wind_gust,
+			clouds,
+			dew_point,
+			uvi,
+			visibility,
 		},
 	} = useAppContextState();
 	const { setSettingsOpen } = useAppContextDispatch();
 	const [backgroundImageURL, setBackgroundImageURL] = useState(null);
+	const [showmore, setShowMore] = useState(false);
 	const [slideDown, slideDownSet] = useSpring(() => ({
 		from: { opacity: 0, transform: 'translate3d(0, -20%, 0)' },
 		to: { opacity: 1, transform: 'translate3d(0, 0, 0)' },
@@ -130,35 +132,100 @@ function Home() {
 								<td className='px-2'>{pressure} mbar</td>
 							</tr>
 							<tr>
-								<td className='px-2'>Wind Speed</td>
-								<td className='px-2'>
-									{unit === 'imperial'
-										? wind_speed + ' mi/hr'
-										: wind_speed + ' m/s'}
-								</td>
+								<td className='px-2'>Clouds</td>
+								<td className='px-2'>{clouds}%</td>
 							</tr>
 							<tr>
-								<td className='px-2'>Wind Direction</td>
-								<td className='px-2'>{wind_deg}&deg;</td>
+								<td className='px-2'>Visibility</td>
+								<td className='px-2'>{visibility} m</td>
 							</tr>
 							<tr>
-								<td className='px-2'>Sunrise</td>
-								<td className='px-2'>
-									{timeFormat === 12
-										? dayjs(sunrise * 1000).format('hh:mm A')
-										: dayjs(sunrise * 1000).format('HH:mm')}
-								</td>
+								<td className='px-2'>Dew Point</td>
+								<td className='px-2'>{dew_point}&deg;</td>
 							</tr>
 							<tr>
-								<td className='px-2'>Sunset</td>
-								<td className='px-2'>
-									{timeFormat === 12
-										? dayjs(sunset * 1000).format('hh:mm A')
-										: dayjs(sunset * 1000).format('HH:mm')}
-								</td>
+								<td className='px-2'>UV Index</td>
+								<td className='px-2'>{uvi}</td>
 							</tr>
+							{showmore && (
+								<>
+									<tr>
+										<td className='px-2'>Wind Speed</td>
+										<td className='px-2'>
+											{unit === 'imperial'
+												? wind_speed + ' mi/hr'
+												: wind_speed + ' m/s'}
+										</td>
+									</tr>
+									{wind_gust && (
+										<tr>
+											<td className='px-2'>Wind Gust</td>
+											<td className='px-2'>
+												{unit === 'imperial'
+													? wind_gust + ' mi/hr'
+													: wind_gust + ' m/s'}
+											</td>
+										</tr>
+									)}
+									<tr>
+										<td className='px-2'>Wind Direction</td>
+										<td className='px-2'>{wind_deg}&deg;</td>
+									</tr>
+									<tr>
+										<td className='px-2'>Sunrise</td>
+										<td className='px-2'>
+											{timeFormat === 12
+												? dayjs(sunrise * 1000).format('hh:mm A')
+												: dayjs(sunrise * 1000).format('HH:mm')}
+										</td>
+									</tr>
+									<tr>
+										<td className='px-2'>Sunset</td>
+										<td className='px-2'>
+											{timeFormat === 12
+												? dayjs(sunset * 1000).format('hh:mm A')
+												: dayjs(sunset * 1000).format('HH:mm')}
+										</td>
+									</tr>
+								</>
+							)}
 						</tbody>
 					</table>
+					<button
+						className='flex items-center px-2 my-2'
+						onClick={() => setShowMore((prevState) => !prevState)}>
+						{showmore ? (
+							<>
+								Less
+								<svg
+									className='w-4 h-4 mx-1'
+									fill='currentColor'
+									viewBox='0 0 20 20'
+									xmlns='http://www.w3.org/2000/svg'>
+									<path
+										fillRule='evenodd'
+										d='M4.293 15.707a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414 0zm0-6a1 1 0 010-1.414l5-5a1 1 0 011.414 0l5 5a1 1 0 01-1.414 1.414L10 5.414 5.707 9.707a1 1 0 01-1.414 0z'
+										clipRule='evenodd'
+									/>
+								</svg>
+							</>
+						) : (
+							<>
+								More
+								<svg
+									className='w-4 h-4 mx-1'
+									fill='currentColor'
+									viewBox='0 0 20 20'
+									xmlns='http://www.w3.org/2000/svg'>
+									<path
+										fillRule='evenodd'
+										d='M15.707 4.293a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 011.414-1.414L10 8.586l4.293-4.293a1 1 0 011.414 0zm0 6a1 1 0 010 1.414l-5 5a1 1 0 01-1.414 0l-5-5a1 1 0 111.414-1.414L10 14.586l4.293-4.293a1 1 0 011.414 0z'
+										clipRule='evenodd'
+									/>
+								</svg>
+							</>
+						)}
+					</button>
 				</animated.section>
 			</div>
 
