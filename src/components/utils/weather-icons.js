5474bb4dd2b7880ab60abbaebb24589ad3e57e21
Abdul Samad <Samk8778@yahoo.com>
2020-09-13 13:25:43 +0530

    :art: Added weather icons export

---
+++ src/components/utils/weather-icons.js
@@ -0,0 +1,39 @@
+import _01d from '../../assets/01d.png';
+import _01n from '../../assets/01n.png';
+import _02d from '../../assets/02d.png';
+import _02n from '../../assets/02n.png';
+import _03d from '../../assets/03d.png';
+import _03n from '../../assets/03n.png';
+import _04d from '../../assets/04d.png';
+import _04n from '../../assets/04n.png';
+import _09d from '../../assets/09d.png';
+import _09n from '../../assets/09n.png';
+import _10d from '../../assets/10d.png';
+import _10n from '../../assets/10n.png';
+import _11d from '../../assets/11d.png';
+import _11n from '../../assets/11n.png';
+import _13d from '../../assets/13d.png';
+import _13n from '../../assets/13n.png';
+import _50d from '../../assets/50d.png';
+import _50n from '../../assets/50n.png';
+
+export {
+	_01d,
+	_01n,
+	_02d,
+	_02n,
+	_03d,
+	_03n,
+	_04d,
+	_04n,
+	_09d,
+	_09n,
+	_10d,
+	_10n,
+	_11d,
+	_11n,
+	_13d,
+	_13n,
+	_50d,
+	_50n,
+};
