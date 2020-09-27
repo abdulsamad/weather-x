cc5ef0b2637a8187d2e879ea60f0ff091e01d13d
Abdul Samad <Samk8778@yahoo.com>
2020-09-27 14:01:28 +0530

    :bento: Added background images

---
+++ src/components/utils/background-images.js
@@ -0,0 +1,23 @@
+import background_1 from '../../assets/backgrounds/background-1.jpg';
+import background_2 from '../../assets/backgrounds/background-2.jpg';
+import background_3 from '../../assets/backgrounds/background-3.jpg';
+import background_4 from '../../assets/backgrounds/background-4.jpg';
+import background_5 from '../../assets/backgrounds/background-5.jpg';
+import background_6 from '../../assets/backgrounds/background-6.jpg';
+import background_7 from '../../assets/backgrounds/background-7.jpg';
+import background_8 from '../../assets/backgrounds/background-8.jpg';
+import background_9 from '../../assets/backgrounds/background-9.jpg';
+import background_10 from '../../assets/backgrounds/background-10.jpg';
+
+export {
+	background_1,
+	background_2,
+	background_3,
+	background_4,
+	background_5,
+	background_6,
+	background_7,
+	background_8,
+	background_9,
+	background_10,
+};
