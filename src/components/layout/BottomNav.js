60c97170eb5d24b7d56157506f32c1092a738b66
Abdul Samad <Samk8778@yahoo.com>
2020-10-03 16:19:18 +0530

    :art: Corrected the class attribute mistake

--- src/components/layout/BottomNav.js
+++ src/components/layout/BottomNav.js
@@ -21,7 +21,7 @@ function BottomNav() {
 					xmlns='http://www.w3.org/2000/svg'>
 					<path d='M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z' />
 				</svg>
-				<span class='sr-only'>Home</span>
+				<span className='sr-only'>Home</span>
 			</NavLink>
 			<NavLink
 				exact
@@ -39,7 +39,7 @@ function BottomNav() {
 						clipRule='evenodd'
 					/>
 				</svg>
-				<span class='sr-only'>Next 48 Hours</span>
+				<span className='sr-only'>Next 48 Hours</span>
 			</NavLink>
 			<NavLink
 				exact
@@ -57,7 +57,7 @@ function BottomNav() {
 						clipRule='evenodd'
 					/>
 				</svg>
-				<span class='sr-only'>Next 7 Days</span>
+				<span className='sr-only'>Next 7 Days</span>
 			</NavLink>
 			<NavLink
 				exact
@@ -71,7 +71,7 @@ function BottomNav() {
 					xmlns='http://www.w3.org/2000/svg'>
 					<path d='M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z' />
 				</svg>
-				<span class='sr-only'>Stats</span>
+				<span className='sr-only'>Stats</span>
 			</NavLink>
 		</nav>
 	);
