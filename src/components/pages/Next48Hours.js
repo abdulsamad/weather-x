32f39feb45cb592d523429dc746129e492364319
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-09 22:22:36 +0530

    :dizzy: Removed animation from next days

--- src/components/pages/Next48Hours.js
+++ src/components/pages/Next48Hours.js
@@ -1,30 +1,17 @@
-import React, { Fragment, useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useTrail, animated } from 'react-spring';
-import dayjs from 'dayjs';
-import { useAppContextDispatch, useAppContextState } from '../../context/context';
-import * as icons from '../utils/weather-icons';
+import React, { Fragment, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import dayjs from "dayjs";
+import {
+  useAppContextDispatch,
+  useAppContextState,
+} from "../../context/context";
+import * as icons from "../utils/weather-icons";
 
 function Next48Hours() {
-  const { unit, next48Hours, timeFormat, loading, place, alert } = useAppContextState();
+  const { unit, next48Hours, timeFormat, loading, place, alert } =
+    useAppContextState();
   const { findByName, setPlace } = useAppContextDispatch();
   const { city } = useParams();
-
-  const [trail, setTrail] = useTrail(next48Hours.length, () => ({
-    config: {
-      mass: 1,
-      tension: 120,
-      friction: 14,
-    },
-    from: {
-      opacity: 0,
-      transform: 'translateY(8px)',
-    },
-    to: {
-      opacity: 1,
-      transform: 'translateY(0)',
-    },
-  }));
 
   useEffect(() => {
     if (place !== city) {
@@ -32,7 +19,6 @@ function Next48Hours() {
       setPlace(city);
     }
 
-    setTrail({});
     // eslint-disable-next-line
   }, []);
 
@@ -41,8 +27,9 @@ function Next48Hours() {
       <div
         className=" w-screen flex justify-center items-center"
         style={{
-          height: 'calc(100vh - 60px)',
-        }}>
+          height: "calc(100vh - 60px)",
+        }}
+      >
         <div className="loader animate-spin w-12 h-12 border-4 border-blue-500 rounded-full"></div>
       </div>
     );
@@ -52,65 +39,75 @@ function Next48Hours() {
     <div
       className="h-full w-screen text-white bg-fixed bg-no-repeat bg-gradient-to-b from-blue-500 to-red-600 px-5"
       style={{
-        height: 'calc(100vh - 60px)',
-      }}>
+        height: "calc(100vh - 60px)",
+      }}
+    >
       <div className="vertical-scroll h-full w-full overflow-auto container md:px-2 mx-auto">
         <h2 className="text-center font-bold my-5 text-lg">Next 48h</h2>
         {next48Hours &&
-          next48Hours.map(({ weather, wind_speed, wind_deg, temp, dt }, index) => (
-            <Fragment key={dt}>
-              {index === 0 && dayjs(dt * 1000).hour() !== 0 && (
-                <h3 className="text-center uppercase mb-2 mt-4 font-semibold">
-                  {dayjs(dt * 1000).format('dddd')}&nbsp;
-                  <span className="text-light">{dayjs(dt * 1000).format('DD.MM')}</span>
-                </h3>
-              )}
-              {dayjs(dt * 1000).hour() === 0 && (
-                <h3 className="text-center uppercase mb-2 mt-4 font-semibold">
-                  {dayjs(dt * 1000).format('dddd')}&nbsp;
-                  <span className="text-light">{dayjs(dt * 1000).format('DD.MM')}</span>
-                </h3>
-              )}
-              <animated.div
-                style={trail[index]}
-                className="flex bg-gray-100 bg-opacity-25 rounded-lg p-2 my-2">
-                <div className="w-1/2 flex flex-col items-start my-1">
-                  <span className="font-semibold h-8 flex items-center text-xl">
-                    {timeFormat === 12
-                      ? dayjs(dt * 1000).format('hh:mm A')
-                      : dayjs(dt * 1000).format('HH:mm')}
-                  </span>
-                  <span className="text-lg capitalize">{weather[0].description}</span>
-                </div>
-                <div className="w-1/4 flex flex-col items-center text-center my-1 text-lg">
-                  <div className="h-8">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                      className="w-6 h-6 fill-current"
-                      style={{
-                        transform: `rotateZ(${wind_deg}deg)`,
-                      }}>
-                      <path d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z" />
-                    </svg>
+          next48Hours.map(
+            ({ weather, wind_speed, wind_deg, temp, dt }, index) => (
+              <Fragment key={dt}>
+                {index === 0 && dayjs(dt * 1000).hour() !== 0 && (
+                  <h3 className="text-center uppercase mb-2 mt-4 font-semibold">
+                    {dayjs(dt * 1000).format("dddd")}&nbsp;
+                    <span className="text-light">
+                      {dayjs(dt * 1000).format("DD.MM")}
+                    </span>
+                  </h3>
+                )}
+                {dayjs(dt * 1000).hour() === 0 && (
+                  <h3 className="text-center uppercase mb-2 mt-4 font-semibold">
+                    {dayjs(dt * 1000).format("dddd")}&nbsp;
+                    <span className="text-light">
+                      {dayjs(dt * 1000).format("DD.MM")}
+                    </span>
+                  </h3>
+                )}
+                <div className="flex bg-gray-100 bg-opacity-25 rounded-lg p-2 my-2">
+                  <div className="w-1/2 flex flex-col items-start my-1">
+                    <span className="font-semibold h-8 flex items-center text-xl">
+                      {timeFormat === 12
+                        ? dayjs(dt * 1000).format("hh:mm A")
+                        : dayjs(dt * 1000).format("HH:mm")}
+                    </span>
+                    <span className="text-lg capitalize">
+                      {weather[0].description}
+                    </span>
                   </div>
-                  <span className="truncate">
-                    {unit === 'imperial' ? wind_speed + ' mi/hr' : wind_speed + ' m/s'}
-                  </span>
+                  <div className="w-1/4 flex flex-col items-center text-center my-1 text-lg">
+                    <div className="h-8">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="24"
+                        viewBox="0 0 24 24"
+                        className="w-6 h-6 fill-current"
+                        style={{
+                          transform: `rotateZ(${wind_deg}deg)`,
+                        }}
+                      >
+                        <path d="M24 0l-6 22-8.129-7.239 7.802-8.234-10.458 7.227-7.215-1.754 24-12zm-15 16.668v7.332l3.258-4.431-3.258-2.901z" />
+                      </svg>
+                    </div>
+                    <span className="truncate">
+                      {unit === "imperial"
+                        ? wind_speed + " mi/hr"
+                        : wind_speed + " m/s"}
+                    </span>
+                  </div>
+                  <div className="w-1/4 flex flex-col items-center my-1 text-lg">
+                    <span className="">{temp}&deg;</span>
+                    <img
+                      src={icons["_" + weather[0].icon]}
+                      className="h-8 transform scale-150"
+                      alt="weather icon"
+                    />
+                  </div>
                 </div>
-                <div className="w-1/4 flex flex-col items-center my-1 text-lg">
-                  <span className="">{temp}&deg;</span>
-                  <img
-                    src={icons['_' + weather[0].icon]}
-                    className="h-8 transform scale-150"
-                    alt="weather icon"
-                  />
-                </div>
-              </animated.div>
-            </Fragment>
-          ))}
+              </Fragment>
+            )
+          )}
       </div>
     </div>
   );
--- src/components/pages/Next7Days.js
+++ src/components/pages/Next7Days.js
@@ -1,30 +1,16 @@
-import React, { useEffect } from 'react';
-import { useParams } from 'react-router-dom';
-import { useTrail, animated } from 'react-spring';
-import dayjs from 'dayjs';
-import { useAppContextDispatch, useAppContextState } from '../../context/context';
-import * as icons from '../utils/weather-icons';
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import dayjs from "dayjs";
+import {
+  useAppContextDispatch,
+  useAppContextState,
+} from "../../context/context";
+import * as icons from "../utils/weather-icons";
 
 function Next7Days() {
   const { next7Days, loading, place, unit, alert } = useAppContextState();
   const { setPlace, findByName } = useAppContextDispatch();
   const { city } = useParams();
-
-  const [trail, setTrail] = useTrail(next7Days.length, () => ({
-    config: {
-      mass: 1,
-      tension: 120,
-      friction: 14,
-    },
-    from: {
-      opacity: 0,
-      transform: 'translateY(8px)',
-    },
-    to: {
-      opacity: 1,
-      transform: 'translateY(0)',
-    },
-  }));
 
   useEffect(() => {
     if (place !== city) {
@@ -32,7 +18,6 @@ function Next7Days() {
       setPlace(city);
     }
 
-    setTrail({});
     // eslint-disable-next-line
   }, []);
 
@@ -41,8 +26,9 @@ function Next7Days() {
       <div
         className=" w-screen flex justify-center items-center"
         style={{
-          height: 'calc(100vh - 60px)',
-        }}>
+          height: "calc(100vh - 60px)",
+        }}
+      >
         <div className="loader animate-spin w-12 h-12 border-4 border-blue-500 rounded-full"></div>
       </div>
     );
@@ -52,32 +38,49 @@ function Next7Days() {
     <div
       className="h-full w-screen text-white bg-gradient-to-b bg-no-repeat from-purple-600 to-teal-500 relative px-5"
       style={{
-        height: 'calc(100vh - 60px)',
-      }}>
+        height: "calc(100vh - 60px)",
+      }}
+    >
       <div className="vertical-scroll h-full w-full overflow-auto container md:px-2 mx-auto">
         <h2 className="text-center font-bold text-lg my-5">Next 7 days</h2>
         {next7Days.map(
-          ({ dt, weather, temp, pressure, humidity, uvi, wind_speed, wind_deg }, index) => (
-            <animated.div
+          (
+            {
+              dt,
+              weather,
+              temp,
+              pressure,
+              humidity,
+              uvi,
+              wind_speed,
+              wind_deg,
+            },
+            index
+          ) => (
+            <div
               key={dt}
-              style={trail[index]}
-              className="bg-gray-100 bg-opacity-25 p-5 rounded-lg my-2">
+              className="bg-gray-100 bg-opacity-25 p-5 rounded-lg my-2"
+            >
               {
                 <h3 className="text-center uppercase mb-2 font-semibold">
-                  {dayjs(dt * 1000).format('dddd')}&nbsp;
-                  <span className="text-light">{dayjs(dt * 1000).format('DD.MM')}</span>
+                  {dayjs(dt * 1000).format("dddd")}&nbsp;
+                  <span className="text-light">
+                    {dayjs(dt * 1000).format("DD.MM")}
+                  </span>
                 </h3>
               }
               <div className="flex justify-between">
                 <span className="capitalize">{weather[0].description}</span>
                 <img
-                  src={icons['_' + weather[0].icon]}
+                  src={icons["_" + weather[0].icon]}
                   className="w-8 h-8 transform scale-150"
                   alt="weather icon"
                 />
                 <span className="font-semibold">
-                  {parseInt(temp.max)}&deg;{' '}
-                  <span className="font-light text-sm ">{parseInt(temp.min)}&deg;</span>
+                  {parseInt(temp.max)}&deg;{" "}
+                  <span className="font-light text-sm ">
+                    {parseInt(temp.min)}&deg;
+                  </span>
                 </span>
               </div>
               <div className="flex my-2">
@@ -98,12 +101,14 @@ function Next7Days() {
                   </div>
                   <div>
                     <strong className="semibold">Wind: </strong>
-                    {unit === 'imperial' ? wind_speed + ' mi/hr' : wind_speed + ' m/s'}
+                    {unit === "imperial"
+                      ? wind_speed + " mi/hr"
+                      : wind_speed + " m/s"}
                   </div>
                 </div>
               </div>
-            </animated.div>
-          ),
+            </div>
+          )
         )}
       </div>
     </div>
